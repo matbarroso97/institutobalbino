692ac8458921126af042fb12ca25151e9d8a7fe1
matbarroso97 <matheusbarrosohomeoffice@gmail.com>
2024-09-03 23:16:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Exemplo de funcionalidade JavaScript para o botão de doação
+document.getElementById('donateButton').addEventListener('click', function() {
+    alert('Obrigado por considerar uma doação!');
+});
+
+
