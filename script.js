542db95bfc0866c69d40d843f82462676a99dbf2
matbarroso97 <matheusbarrosohomeoffice@gmail.com>
2024-09-04 02:41:50 -0300

    add

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ document.getElementById('donateButton').addEventListener('click', function() {
 });
 
 
+// Função para verificar se o elemento está visível na tela
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Função para adicionar a classe de animação quando o elemento estiver visível
+function handleScroll() {
+    const elements = document.querySelectorAll('.fade-in-up');
+    elements.forEach(element => {
+        if (isElementInViewport(element)) {
+            element.classList.add('animated');
+        }
+    });
+}
+
+// Adicionar evento de scroll para verificar a visibilidade
+window.addEventListener('scroll', handleScroll);
+
+// Aplicar a animação inicial para os elementos visíveis no carregamento da página
+window.addEventListener('load', handleScroll);
